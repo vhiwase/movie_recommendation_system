3152fe01884725914043f1074c736102b82f0e6d
Vaibhav Hiwase <hiwase.vaibhav@gmail.com>
2021-05-12 15:05:52 +0530

    fix

--- public_ip.py
+++ public_ip.py
@@ -1,14 +1,19 @@
 import subprocess
 import pathlib
+import click
 
 FILE_PATH = pathlib.PosixPath(__file__)
 
 BASE_PATH = FILE_PATH.parent
 file = (BASE_PATH/"project/www/html/webdictionary.txt").absolute().as_posix()
 
-def get_public_ip():
+@click.command()
+@click.option('--localhost', '-L', prompt='Bool', help='localhost')
+def get_public_ip(localhost):
     bashCommand = """host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}'"""
     my_public_ip = subprocess.getoutput(bashCommand)
+    if localhost:
+        my_public_ip = "localhost"
     with open(file, 'w') as f:
         f.write("http://{}:5004/home".format(my_public_ip))
     return my_public_ip
