9d51e4bd526b1d363baa10ac2fde427a335dec8f
Vaibhav Hiwase <hiwase.vaibhav@gmail.com>
2021-05-12 13:53:04 +0530

    minor fix

--- public_ip.py
+++ public_ip.py
@@ -10,14 +10,14 @@ def get_public_ip():
     bashCommand = """host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}'"""
     my_public_ip = subprocess.getoutput(bashCommand)
     with open(file, 'w') as f:
-        f.write("http://{}:5000/home".format(my_public_ip))
+        f.write("http://{}:5004/home".format(my_public_ip))
     return my_public_ip
 
 if __name__ == '__main__':
     my_public_ip = get_public_ip()
     print(my_public_ip)
     with open(file, 'w') as f:
-        f.write("http://{}:5000/home".format(my_public_ip))
+        f.write("http://{}:5004/home".format(my_public_ip))
         
         
         
